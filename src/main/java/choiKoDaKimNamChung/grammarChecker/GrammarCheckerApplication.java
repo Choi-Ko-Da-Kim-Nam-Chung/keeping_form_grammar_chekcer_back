341793ce4daf31ae21b26be5db9249e97e7bff66
chtw2001 <jungtaekwon1019@gmail.com>
2024-03-25 23:23:40 +0900

    [init] initialize setting

---
+++ src/main/java/choiKoDaKimNamChung/grammarChecker/GrammarCheckerApplication.java
@@ -0,0 +1,13 @@
+package choiKoDaKimNamChung.grammarChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrammarCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrammarCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/choiKoDaKimNamChung/grammarChecker/GrammarCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package choiKoDaKimNamChung.grammarChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrammarCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
