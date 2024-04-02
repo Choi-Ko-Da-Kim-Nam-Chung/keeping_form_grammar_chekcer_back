3e3117ad49a163200cff4eb112f44b975a08b7ce
un-known0 <nam0221@naver.com>
2024-04-02 18:17:29 +0900

    [add] DocxApplyImp의 iBodyParse 초안 작성

--- src/main/java/choiKoDaKimNamChung/grammarChecker/service/docx/DocxApplyImp.java
+++ src/main/java/choiKoDaKimNamChung/grammarChecker/service/docx/DocxApplyImp.java
@@ -1,21 +1,32 @@
 package choiKoDaKimNamChung.grammarChecker.service.docx;
 
-import choiKoDaKimNamChung.grammarChecker.docx.Docx;
+import choiKoDaKimNamChung.grammarChecker.docx.*;
 import choiKoDaKimNamChung.grammarChecker.docx.IBody;
-import choiKoDaKimNamChung.grammarChecker.docx.Paragraph;
-import choiKoDaKimNamChung.grammarChecker.docx.Table;
 import org.apache.poi.xwpf.usermodel.*;
 
-import java.util.List;
+import java.util.*;
 
 public class DocxApplyImp implements DocxApply{
     @Override
     public XWPFDocument docxParse(XWPFDocument document, Docx docx) {
+        //header
+        List<IBodyElement> bodyElements = document.getBodyElements();
+        for(int i=0; i<docx.getBody().size(); i++){
+            iBodyParse(bodyElements.get(i), docx.getBody().get(i));
+        }
+        //footer
         return null;
     }
 
     @Override
     public void iBodyParse(IBodyElement bodyElement, IBody iBody) {
+        if (iBody.getType() == IBodyType.PARAGRAPH){
+            paragraphParse((XWPFParagraph) bodyElement,(Paragraph) iBody);
+        }else if(iBody.getType() == IBodyType.TABLE){
+            tableParse((XWPFTable) bodyElement,(Table) iBody);
+        }else{
+
+        }
 
     }
 
