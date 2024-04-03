db1ffa01a5d1b98cf06d83afe2bc6919d566eb59
un-known0 <nam0221@naver.com>
2024-04-03 18:51:19 +0900

    [fix] 잡다한 print 함수 제거

--- src/main/java/choiKoDaKimNamChung/grammarChecker/service/docx/DocxApplyImp.java
+++ src/main/java/choiKoDaKimNamChung/grammarChecker/service/docx/DocxApplyImp.java
@@ -55,26 +55,18 @@ public class DocxApplyImp implements DocxApply{
         int errorIdx = p.getErrors().size()-1;
         int charIdx = paragraph.getText().length();
         while(errorIdx>=0){
-            System.out.println("paragraph = " + paragraph.getRuns().get(runIdx).text());
             if(p.getErrors().get(errorIdx).getReplaceStr() == null){
                 errorIdx--;
                 continue;
             }
-            System.out.println("p.getErrors().get(errorIdx).getOrgStr() = " + p.getErrors().get(errorIdx).getOrgStr());
             while(charIdx - paragraph.getRuns().get(runIdx).text().length() > p.getErrors().get(errorIdx).getEnd() - 1) {
                 charIdx -= paragraph.getRuns().get(runIdx).text().length();
                 runIdx--;
             }
 
-            System.out.println("(charIdx - paragraph.getRuns().get(runIdx).text().length()) = " + (charIdx - paragraph.getRuns().get(runIdx).text().length()));
-            System.out.println("(p.getErrors().get(errorIdx).getStart()) = " + (p.getErrors().get(errorIdx).getStart()));
-            System.out.println("charIdx = " + charIdx);
-            System.out.println("(p.getErrors().get(errorIdx).getEnd()) = " + (p.getErrors().get(errorIdx).getEnd()));
             if(charIdx - paragraph.getRuns().get(runIdx).text().length() > p.getErrors().get(errorIdx).getStart() && charIdx > p.getErrors().get(errorIdx).getEnd()){
                 int length = paragraph.getRuns().get(runIdx).text().length();
-                System.out.println("paragraph.getRuns().get(runIdx).text().substring(paragraph.getRuns().get(runIdx).text().length() - (charIdx - p.getErrors().get(errorIdx).getEnd())) = " + paragraph.getRuns().get(runIdx).text().substring(paragraph.getRuns().get(runIdx).text().length() - (charIdx - p.getErrors().get(errorIdx).getEnd())));
                 paragraph.getRuns().get(runIdx).setText(paragraph.getRuns().get(runIdx).text().substring(paragraph.getRuns().get(runIdx).text().length() - (charIdx - p.getErrors().get(errorIdx).getEnd())),0);
-                System.out.println("paragraph.getRuns().get(runIdx).text() = " + paragraph.getRuns().get(runIdx).text());
                 runIdx--;
                 charIdx -= length;
             }
@@ -90,13 +82,7 @@ public class DocxApplyImp implements DocxApply{
             if(error.getEnd() < charIdx){
                 change = origin.substring(0,origin.length() - (charIdx - error.getStart())) + error.getReplaceStr() + origin.substring(origin.length() - (charIdx - error.getEnd()));
             }else{
-                System.out.println("error.getEnd() = " + error.getEnd());
-                System.out.println("charIdx = " + charIdx);
-                System.out.println("origin = " + origin);
-                System.out.println("error.getReplaceStr() = " + error.getReplaceStr());
-
                 change = origin.substring(0,origin.length() - (charIdx - error.getStart())) + error.getReplaceStr();
-                System.out.println("change = " + change);
             }
             paragraph.getRuns().get(runIdx).setText(change,0);
         }
