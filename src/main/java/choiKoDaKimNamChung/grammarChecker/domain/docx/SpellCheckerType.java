37d42d01595aca58178f8d93a9d48dc5c2ac824a
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-05-19 15:36:49 +0900

    Create SpellCheckerType.java

---
+++ src/main/java/choiKoDaKimNamChung/grammarChecker/domain/docx/SpellCheckerType.java
@@ -0,0 +1,9 @@
+@AllArgsConstructor
+@Getter
+public enum SpellCheckerType {
+    BUSAN_UNIV("http://10.0.134.104:8000/busan"),
+    INCRUIT("http://10.0.134.104:8000/incruit"),
+    JOB_KOREA("http://10.0.134.104:8000/jobkorea");
+
+    private String url;
+}
