f0f25f7aacfa05411b6f6b03d615673b3fe65d73
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-05-20 16:18:30 +0900

    Dev에 반영된 CORS 설정을 Main 브렌치에 반영 (#56)
    * [fix] 매개변수로 새로운 파일명 받는 로직 제거(프론트엔드에서 구현)
    
    * :sparkles: CorsConfig 설정 및 추가
    
    * :pencil2: SpellCheckerType 주소 수정
    
    ---------
    
    Co-authored-by: chtw2001 <jungtaekwon1019@gmail.com>
    Co-authored-by: chtw2001 <116997285+chtw2001@users.noreply.github.com>

--- src/main/java/choiKoDaKimNamChung/grammarChecker/domain/docx/SpellCheckerType.java
+++ src/main/java/choiKoDaKimNamChung/grammarChecker/domain/docx/SpellCheckerType.java
@@ -9,6 +9,5 @@ public enum SpellCheckerType {
     BUSAN_UNIV("http://10.0.134.104:8000/busan"),
     INCRUIT("http://10.0.134.104:8000/incruit"),
     JOB_KOREA("http://10.0.134.104:8000/jobkorea");
-
     private String url;
 }
