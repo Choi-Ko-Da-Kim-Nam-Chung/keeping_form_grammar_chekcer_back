6eb82e8ce8db539e26bf2bbc7d43866f4eeb2b22
aj4941@naver.com <aj4941@naver.com>
2024-05-20 16:16:27 +0900

    :sparkles: CorsConfig 설정 및 추가

---
+++ src/main/java/choiKoDaKimNamChung/grammarChecker/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package choiKoDaKimNamChung.grammarChecker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:3000",
+                "http://spell-checker.co.kr",
+                "https://spell-checker.co.kr",
+                "http://api.spell-checker.co.kr",
+                "https://api.spell-checker.co.kr"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
