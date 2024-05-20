573ba6ddbdd8590256be06ebc97f69d3508ece0b
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-05-20 17:14:26 +0900

    CORS 임시 수정 (#57)
    * :sparkles: CorsConfig 설정 및 추가
    
    * :pencil2: SpellCheckerType 주소 수정
    
    * :pencil2: CORS 임시 수정

--- src/main/java/choiKoDaKimNamChung/grammarChecker/config/CorsConfig.java
+++ src/main/java/choiKoDaKimNamChung/grammarChecker/config/CorsConfig.java
@@ -14,12 +14,12 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("http://localhost:3000",
                 "http://spell-checker.co.kr",
                 "https://spell-checker.co.kr",
                 "http://api.spell-checker.co.kr",
-                "https://api.spell-checker.co.kr"));
+                "https://api.spell-checker.co.kr", "*"));
+      
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
