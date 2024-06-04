82168f3b8b456db4205591c4872911886c2277d3
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-06-04 19:42:57 +0900

    Update SpellCheckerType.java

--- src/main/java/choiKoDaKimNamChung/grammarChecker/domain/docx/SpellCheckerType.java
+++ src/main/java/choiKoDaKimNamChung/grammarChecker/domain/docx/SpellCheckerType.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum SpellCheckerType {
-    BUSAN_UNIV("http://10.0.134.104:8000/busan"),
-    INCRUIT("http://10.0.134.104:8000/incruit"),
-    JOB_KOREA("http://10.0.134.104:8000/jobkorea");
+    BUSAN_UNIV("http://34.64.157.92:8000/busan"),
+    INCRUIT("http://34.64.157.92:8000/incruit"),
+    JOB_KOREA("http://34.64.157.92:8000/jobkorea");
     private String url;
 }
