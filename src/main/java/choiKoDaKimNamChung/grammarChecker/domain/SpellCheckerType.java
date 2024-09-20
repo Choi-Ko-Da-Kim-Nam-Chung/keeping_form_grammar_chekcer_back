d8a6fdf654c06675acc55c103edd25280c5b02cf
chtw2001 <116997285+chtw2001@users.noreply.github.com>
2024-09-20 17:34:08 +0900

    Update SpellCheckerType.java

--- src/main/java/choiKoDaKimNamChung/grammarChecker/domain/SpellCheckerType.java
+++ src/main/java/choiKoDaKimNamChung/grammarChecker/domain/SpellCheckerType.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum SpellCheckerType {
-    BUSAN_UNIV("http://3.34.51.88:8000/busan"),
-    INCRUIT("http://3.34.51.88:8000/incruit"),
-    JOB_KOREA("http://3.34.51.88:8000/jobkorea");
+    BUSAN_UNIV("http://3.39.130.18:8000/busan"),
+    INCRUIT("http://3.39.130.18:8000/incruit"),
+    JOB_KOREA("http://3.39.130.18:8000/jobkorea");
     private String url;
 }
